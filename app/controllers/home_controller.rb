bd8b08f1331ada2e15534e4f0dc16a8885380210
Christopher Pelnar <christopherpelnar@gmail.com>
2016-06-11 12:03:30 -0400

    Added basic routing to handle kennel and customer sign-in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,12 @@
 class HomeController < ApplicationController
+
+  def index
+    if !current_user.nil?
+      if current_user.kennel_or_customer == "kennel"
+        redirect_to kennel_dashboard_path
+      elsif current_user.kennel_or_customer == "customer"
+        redirect_to customer_dashboard_path
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :home
   root to: "home#index"
+  get "/kennel_dashboard", to: "home#kennel_dashboard", as: :kennel_dashboard
+  get "/customer_dashboard", to: "home#customer_dashboard", as: :customer_dashboard
 end
