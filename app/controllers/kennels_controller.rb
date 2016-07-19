5b4b9366c46285138ba9d4d252e51175c2403c25
Christopher Pelnar <christopherpelnar@gmail.com>
2016-07-19 18:23:31 -0400

    Kennel Calendar

--- app/controllers/kennels_controller.rb
+++ app/controllers/kennels_controller.rb
@@ -22,8 +22,98 @@ class KennelsController < ApplicationController
   def kennel_dashboard
     if !current_user.nil?
       @kennel = Kennel.where(user_id: current_user.id).first
-      # @photo = Photo.where(kennel_id: @kennel.id).first
+      if !@kennel.nil?
+        reservations = Reservation.where(kennel_id: @kennel[:id])
+        @reservations = reservations if !reservations.blank?
+        # @photo = Photo.where(kennel_id: @kennel.id).first
+      end
     end
+  end
+
+  def kennel_reservations
+    get_reservations_info
+    pets_going_home?
+  end
+
+  def pets_going_home?
+    @pets_going_home = []
+    @pets.each do |pi|
+      if pi[:check_out] == params[:date]
+        @pets_going_home << [pi[:id], pi[:check_out]]
+      end
+    end
+
+    if !@pets_going_home.blank?
+      @runs_becoming_available = []
+      @run_names = []
+      @pets_going_home.each do |k, v|
+        pet = Pet.find(k)
+        user = User.where(id: pet[:user_id]).first
+        v = Date.parse(sanitize_date(v))
+        reservation = Reservation.where(kennel_id: params[:kennel_id], user_id: user[:id], check_out: v).first
+        JSON.parse(reservation[:run_ids]).each do |ri|
+          @runs_becoming_available << ri
+        end
+        counter = 0
+        @runs_becoming_available.each do |ra|
+          run = Run.find(ra)
+          if !@run_names.flatten.include? run[:title]
+            counter += 1
+            @run_names << [run[:title], counter]
+          else
+            @run_names.each do |k, v|
+              if k == run[:title]
+                v += 1
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def get_reservations_info
+    @runs_maxed_today = []
+    @runs_with_days_left = []
+    @pets = []
+    all_run_ids = []
+    all_pets = []
+    check_in = []
+    unless params[:reservation_ids].nil?
+      params[:reservation_ids].each do |ri|
+        reservation = Reservation.find(ri.to_i)
+        check_in << reservation[:check_in]
+        JSON.parse(reservation[:run_ids]).each do |r|
+          all_run_ids << r
+        end
+        JSON.parse(reservation[:pet_ids]).each do |pi|
+          all_pets << [pi, reservation[:check_in], reservation[:check_out]]
+        end
+      end
+
+      all_run_ids.length.times do
+        number_of_rooms = all_run_ids.count(all_run_ids[0])
+        run = Run.find(all_run_ids[0])
+        if run[:number_of_rooms] == number_of_rooms
+          @runs_maxed_today << run.title
+        else
+          @runs_with_days_left << [run.title, (run.number_of_rooms - number_of_rooms)]
+        end
+        all_run_ids.delete(all_run_ids[0])
+        break if all_run_ids.blank?
+      end
+
+      all_pets.each do |pi, rci, rco|
+        pet = Pet.find(pi)
+        @pets << {user_id: pet[:user_id], id: pet[:id], name: pet[:name], cat_or_dog: pet[:cat_or_dog], breed: pet[:breed], weight: pet[:weight], check_in: unsanitize_date(rci.to_s), check_out: unsanitize_date(rco.to_s)}
+      end
+    else
+      @no_reservations = true
+    end
+  end
+
+  def kennel_reservation_pet_owner_info
+    @user = User.find(params[:id])
   end
 
   def my_kennel_info
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
   # KennelsController
   resources :kennels, only: [:new, :create]
     get "/kennel_dashboard",   to: "kennels#kennel_dashboard",   as: :kennel_dashboard
+    get "/kennel_reservations", to: "kennels#kennel_reservations", as: :kennel_reservations
+    get "/kennel_reservation_pet_owner_info", to: "kennels#kennel_reservation_pet_owner_info", as: :kennel_reservation_pet_owner_info
     get "/my_kennel_info", to: "kennels#my_kennel_info", as: :my_kennel_info
 
   # CustomersController
