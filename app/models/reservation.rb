50242da16c5e91f1a88e55e2b4aca2c46cc5a24f
Christopher Pelnar <christopherpelnar@gmail.com>
2016-07-29 00:18:52 -0400

    Batch update db when reminder emails sent

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,14 +3,11 @@ class Reservation < ActiveRecord::Base
   belongs_to :kennel
 
   def mark_completed_reservations
+    res_ids = []
     date = Date.today
-    reservations = Reservation.where(completed: false)
-    reservations.each do |res|
-      if res[:check_out_date] == date
-        res[:completed] = "true"
-        res.save!
-      end
-    end
+    reservations = Reservation.where(completed: nil, check_out_date: date)
+    reservations.each { |res| res_ids << res[:id] }
+    Reservation.where(id: res_ids).update_all(completed: "true")
   end
 
   def send_reservation_reminder_emails
@@ -21,27 +18,36 @@ class Reservation < ActiveRecord::Base
 
   def send_three_weeks_before_email_reminder
     emails = []
+    run_ids = []
     reservations = Reservation.where(three_weeks_before_email_reminder: nil).where("DATE(check_in_date) = ?", Date.today-21)
-    reservations.each do |key, val|
-      emails << val if key == "customer_email"
+    reservations.each do |res|
+      emails << res[:customer_email]
+      res_ids << res[:id]
     end
     emails.each { |email| UserMailer.send_three_week_reservation_reminder(email).deliver_now }
+    Reservation.where(id: res_ids).update_all(three_weeks_before_email_reminder: "sent")
   end
 
   def send_one_week_before_email_reminder
     emails = []
+    run_ids = []
     reservations = Reservation.where(one_week_before_email_reminder: nil).where("DATE(check_in_date) = ?", Date.today-7)
-    reservations.each do |key, val|
-      emails << val if key == "customer_email"
+    reservations.each do |res|
+      emails << res[:customer_email]
+      res_ids << res[:id]
     end
     emails.each { |email| UserMailer.send_one_week_reservation_reminder(email).deliver_now }
+    Reservation.where(id: res_ids).update_all(one_week_before_email_reminder: "sent")
   end
 
   def send_day_before_email_reminder
     emails = []
-    reservations = Reservation.where(one_week_before_email_reminder: nil).where("DATE(check_in_date) = ?", Date.today-1)
-    reservations.each do |key, val|
-      emails << val if key == "customer_email"
+    res_ids = []
+    reservations = Reservation.where(day_before_email_reminder: nil).where("DATE(check_in_date) = ?", Date.today-1)
+    reservations.each do |res|
+      emails << res[:customer_email]
+      res_ids << res[:id]
     end
-    emails.each { |email| UserMailer.send_day_before_reservation_reminder(email).deliver_now }  end
+    emails.each { |email| UserMailer.send_day_before_reservation_reminder(email).deliver_now }
+    Reservation.where(id: res_ids).update_all(day_before_email_reminder: "sent")
   end
