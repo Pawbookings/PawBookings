e295500a1c78f2ba51cceac8e95a8113a8d58f80
Christopher Thomas Pelnar <christopherpelnar@gmail.com>
2016-09-18 10:12:50 -0400

    Changed integer to floating point decimal value

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -282,7 +282,7 @@ class PaymentsController < ApplicationController
   def process_payment
     payment = Payment.new
     get_price_total
-    if payment.payment_successful?(params, params[:total_price].to_i)
+    if payment.payment_successful?(params, params[:total_price].to_f)
       get_room_info
       if @kennel.reservations.create(
                                    user_id: @user.id,
