9ccbde7317a480e189b997ae3576c640adc9055e
Christopher Thomas Pelnar <christopherpelnar@gmail.com>
2016-09-30 01:02:43 -0400

    Added styles to searches_show page.

--- app/models/kennel.rb
+++ app/models/kennel.rb
@@ -24,6 +24,6 @@ class Kennel < ActiveRecord::Base
   geocoded_by :zip
   after_validation :geocode
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/default_testimonial_image.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
