49f69bf5c2dc47540c35cc2766bcfa4fbcaa18c8
Christopher Thomas Pelnar <christopherpelnar@gmail.com>
2017-01-07 13:41:45 -0500

    Added error message handeling for devise.

--- app/controllers/devise_registrations_controller.rb
+++ app/controllers/devise_registrations_controller.rb
@@ -30,6 +30,7 @@ class DeviseRegistrationsController < Devise::RegistrationsController
         respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
     else
+      flash[:notice] = resource.errors.full_messages.first
       clean_up_passwords resource
       set_minimum_password_length
       redirect_to request.referrer
