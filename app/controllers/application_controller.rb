e2ddeaba304998464d755a324a4dfae541bd560b
kolisnyklera <kolisnyklera@gmail.com>
2019-05-20 03:43:12 +0300

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ private
 
   def set_time_zone
     if current_user
-      Time.zone = current_user.time_zone if current_user.time_zone
+      Time.zone = current_user.time_zone unless current_user.time_zone.empty?
     end
   end
 
