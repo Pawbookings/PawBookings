53c3ac871d065ae6350b75d064efd60b9398b07f
kolisnyklera <kolisnyklera@gmail.com>
2019-05-25 21:00:20 +0300

    fixes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -8,7 +8,7 @@ class Photo < ActiveRecord::Base
                 :convert_options => { :all => "-quality 100" },
                 url: ":s3_domain_url",
                 path: "/image/:id/:filename",
-                s3_region: "ENV['aws_region']",
+                s3_region: ENV['aws_region'],
                 s3_protocol: :https,
                 default_url: "/images/:style/missing.png",
                 :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
